cab0504a9cda786a2f46219901142f250152dfb6
Carlos Mauricio Cerdeira <mauricio_cerdeira@hotmail.com>
2021-04-08 12:19:46 -0300

    Projects CRUD, Likes

--- src/app.js
+++ src/app.js
@@ -11,11 +11,24 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
@@ -27,7 +40,17 @@ app.delete("/repositories/:id", (request, response) => {
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repository = repositories.find(repository ==> repository.id === id);
+
+  if (!repository) {
+    return response.status(400).send();
+  }
+  
+  repository.likes++;
+
+  return response.json(repository);
 });
 
 module.exports = app;
